8499837462a31bfc9c5401c5bea6331bfdd8c804
netraax <dils389@gmail.com>
2024-12-07 06:37:16 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>VintedGooat</title>
-  </head>
-  <body>
-    <div id="app">
-      <h1>VintedGooat</h1>
-    </div>
-    <script type="module" src="./main.js"></script>
-  </body>
-</html>
+import '../css/style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>VintedGooat</h1>
+    <p>Site en construction</p>
+  </div>
+`
