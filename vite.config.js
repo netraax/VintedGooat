ef6f37c6212594afa9d18530e0203a80102500cd
netraax <dils389@gmail.com>
2024-12-07 18:24:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: 'src',
   base: '/',
+  plugins: [react()],
   build: {
     outDir: '../dist',
     emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
   }
 })
