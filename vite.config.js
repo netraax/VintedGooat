63599be46f8677ff5845e397d0efd3838c10256e
netraax <dils389@gmail.com>
2024-12-07 06:23:20 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,33 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: 'src',
-  base: '/',
+  publicDir: '../public',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
-    sourcemap: true
-  },
-  server: {
-    port: 3000,
-    hot: true
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html')
+      }
+    }
   },
   resolve: {
     alias: {
-      '@': '/src'
+      '@behaviors': resolve(__dirname, './src/behaviors'),
+      '@css': resolve(__dirname, './css')
     }
+  },
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: `@import "@css/style.css";`
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
   }
 })
