cfd872e1b83339f817ed194bdba5faebf12d2a42
netraax <dils389@gmail.com>
2024-12-07 17:34:41 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
-  root: 'src',  // Changé de 'src/html' à 'src'
+  root: 'src',
   base: '/',
   build: {
-    outDir: '../dist', // Changé de '../../dist' à '../dist'
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'), // Changé de 'src/html/index.html'
-      }
-    },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '@behaviors': resolve(__dirname, 'src/behaviors')
-    }
+    outDir: '../dist',
+    emptyOutDir: true
   }
 })
