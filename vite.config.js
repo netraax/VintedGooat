8f7fc354282ef41ca57b0bac34019a1a2efbc4e0
netraax <dils389@gmail.com>
2024-12-07 06:43:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   root: 'src',
   base: '/',
   build: {
     outDir: '../dist',
-    emptyOutDir: true
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@css': '/css'
-    }
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'src/index.html',
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') return 'assets/css/style.css'
+          return 'assets/[name]-[hash][extname]'
+        },
+      },
+    },
   }
 })
