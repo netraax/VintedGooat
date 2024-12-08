556a4df50b68c214da8031e56af4f4236bfc6f5a
netraax <dils389@gmail.com>
2024-12-08 05:21:03 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: 'src',
+  base: '/',
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html')
+      }
+    }
   },
-  base: './'  // Ajout important pour Netlify
+  resolve: {
+    alias: {
+      '@css': resolve(__dirname, './css'),
+      '@behaviors': resolve(__dirname, './src/behaviors')
+    }
+  },
+  server: {
+    port: 3000
+  }
 })
